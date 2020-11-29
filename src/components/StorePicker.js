8fd53ce8df19d54114eb946dbddef6b179c75440
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-11-29 17:33:39 -0300

    Create gotToStore submit handler

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
+    myInput =  React.createRef();
+
+    constructor() {
+        super();
+        this.goToStore =  this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. Get the text from that input
+        console.log(this.myInput);
+        // 3. Change the page to /store/whatever-they-entered
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    placeholder="StoreName"
+                    defaultValue={getFunName()}
+                    required 
+                />
                 <button type="submit">Visit Store -></button>
             </form>
         );
